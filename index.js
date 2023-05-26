5acf6c58b90ab76436d3fcc1deff644643eb5a13
Ayomide Olugbile <ayomideolugbile29@gmail.com>
2023-05-26 16:22:59 +0100

    It was edited

--- index.js
+++ index.js
@@ -23,3 +23,42 @@ const bruhm=()=>{
     Allgoods.push(eachgood)
     localStorage.setItem('allGood',JSON.stringify(Allgoods))
 }
+const samsung=()=>{
+    let eachgood={
+        id:3,
+        img:`<img src="https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/36/7440622/1.jpg?9388" class="card-img-top" alt="...">`,
+        price: 121570,
+        description: `<p class="card-text"><b>XIAOMI Redmi 10C, 6.53-inch 4GB RAM, 128GB ROM - 5000mAh-Grey</b></p>`
+    }
+    Allgoods.push(eachgood)
+    localStorage.setItem('allGood',JSON.stringify(Allgoods))
+}
+const xiaomi=()=>{
+    let eachgood={
+        id:4,
+        img:`<img src="https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/96/6792202/1.jpg?2520" class="card-img-top" alt="...">`,
+        price: 113433,
+        description: `<p class="card-text"><b>XIAOMI Redmi 10C, 6.53-inch 4GB RAM, 128GB ROM - 5000mAh-Grey</b></p>`
+    }
+    Allgoods.push(eachgood)
+    localStorage.setItem('allGood',JSON.stringify(Allgoods))
+}
+
+let getUser=JSON.parse(localStorage.getItem('ecommerce'))
+let userIndex=localStorage.getItem('userIndex')
+if(localStorage.welcome=="welcome"){
+
+}
+else{
+    if(localStorage.ecommerce){
+        Swal.fire(
+            'Welcome',
+            getUser[userIndex].lastname+" "+getUser[userIndex].firstname,
+            'success'
+          )
+          setTimeout(() => {
+            
+              localStorage.setItem('welcome', 'welcome')
+          }, 1000);
+    }
+}
